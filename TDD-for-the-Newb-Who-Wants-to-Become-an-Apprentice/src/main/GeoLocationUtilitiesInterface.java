afe8e796980edda56eea45803cfee6c674285544
Howard Deiner <howard.deiner@deinersoft.com>
2017-01-09 18:46:44 -0600

    Mavenize and IntelliJize

--- TDD-for-the-Newb-Who-Wants-to-Become-an-Apprentice/src/main/GeoLocationUtilitiesInterface.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public interface GeoLocationUtilitiesInterface {
-	public double[] getLatLongFromPostalcode(String postalCode);
-}
--- TDD-for-the-Newb-Who-Wants-to-Become-an-Apprentice/src/main/Merchant.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Merchant extends TransactionEntity {
-
-	public Merchant (String name, String address, String postalCode, String phoneNumber, String eMailAddress) throws Exception {
-		super (name, address, postalCode, phoneNumber, eMailAddress);
-	}
-
-}
